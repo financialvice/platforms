f455f9e349abd4d72d9bb0116c9aed2ceebf598e
financialvice <cam@dubdubdub.xyz>
2023-08-21 13:37:29 -0700

    dermain

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ module.exports = {
       "pbs.twimg.com",
       "avatars.githubusercontent.com",
       "www.google.com",
+      "googleusercontent.com",
       "flag.vercel.app",
       "illustrations.popsy.co",
     ],
